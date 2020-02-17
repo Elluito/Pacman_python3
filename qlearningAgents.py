6812015b49620015ba9f96e9da83cb0be93fb77b
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-02-17 01:27:35 -0500

    Cambios

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -328,7 +328,7 @@ class Policy:
 
         self.width = width
         self.height = height
-        # tf.logging.set_verbosity(tf.logging.ERROR)
+
         self.priority = use_prior
         if use_image:
             self.state_space = (self.height,self.width,1)
@@ -354,7 +354,7 @@ class Policy:
 
             with self.strategy.scope():
                 self.model = keras.Sequential([
-                    keras.layers.Conv2D(32, (3, 3),  input_shape=self.state_space),
+                    keras.layers.Conv2D(32, (3, 3),  input_shape=(self.state_space,)),
                     keras.layers.BatchNormalization(),
                     keras.layers.Activation("relu"),
                     keras.layers.Conv2D(64, (3, 3),strides=[2,2],use_bias=False),
