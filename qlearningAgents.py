dac289f52f268b8693362e8a2e659153ed46818b
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-02-17 14:59:03 -0500

    cambio

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -354,7 +354,7 @@ class Policy:
 
             with self.strategy.scope():
                 self.model = keras.Sequential([
-                    keras.layers.Conv2D(32, (3, 3),  input_shape=(self.state_space,)),
+                    keras.layers.Conv2D(32, (3, 3),  input_shape=self.state_space),
                     keras.layers.BatchNormalization(),
                     keras.layers.Activation("relu"),
                     keras.layers.Conv2D(64, (3, 3),strides=[2,2],use_bias=False),
