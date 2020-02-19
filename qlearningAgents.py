cf0b582ecc49e99770ac17f80da412cdd1bf1e22
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-02-18 23:17:48 -0500

    Cambios

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -51,7 +51,7 @@ resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='alfredoavendan
 tf.config.experimental_connect_to_cluster(resolver)
 tf.tpu.experimental.initialize_tpu_system(resolver)
 strategy = tf.distribute.experimental.TPUStrategy(resolver)
-tf.logging.set_verbosity(tf.logging.INFO)
+
 
 # global  strategy
 
