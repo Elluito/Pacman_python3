a69f7e0c89e74ae0ff5d19c9503905afc13454c7
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-02-20 17:46:43 -0500

    Cambios

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -808,7 +808,7 @@ class QLearningAgent(ReinforcementAgent):
                 # input_fn=tf.compat.v1.estimator.inputs.numpy_input_fn(features.reshape(shape),shuffle=False)
                 features= features.reshape(shape)
                 # features = tf.convert_to_tensor(features,dtype=tf.float32)
-                cosa=list(self.policy_second.model.predict(input_fn = make_predict_fn(features)))
+                cosa=list(self.policy_second.model_action.predict(input_fn = make_predict_fn(features)))
                 print(cosa)
                 # for single_prediction in cosa:
                 #     Q_actual =single_prediction["Q_values"]
