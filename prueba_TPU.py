7e6aad1f83a8a3b5d2a0caa6f56dbaa6258b7e8c
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-02-27 21:39:07 -0500

    Cambios

--- prueba_TPU.py
+++ prueba_TPU.py
@@ -1,203 +1,116 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""MNIST model training using TPUs.
+import distutils
 
-This program demonstrates training of the convolutional neural network model
-defined in mnist.py on Google Cloud TPUs (https://cloud.google.com/tpu/).
-
-If you are not interested in TPUs, you should ignore this file.
-"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import os
-import sys
-
+import numpy as np
 import tensorflow as tf
-# pylint: disable=g-bad-import-order
-from absl import app as absl_app  # pylint: disable=unused-import
 
-# pylint: enable=g-bad-import-order
+if distutils.version.LooseVersion(tf.__version__) < '1.14':
+    raise Exception('This notebook is compatible with TensorFlow 1.14 or higher, for TensorFlow 1.13 or lower please use the previous version at https://github.com/tensorflow/tpu/blob/r1.13/tools/colab/shakespeare_with_tpu_and_keras.ipynb')
 
-# For open source environment, add grandparent directory for import
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(sys.path[0]))))
+# This address identifies the TPU we'll use when configuring TensorFlow.
+TPU_WORKER = 'grpc://10.240.1.2:8470'
 
-from tensorflow.official.r1.mnist import dataset  # pylint: disable=wrong-import-position
-from tensorflow.official.r1.mnist import mnist  # pylint: disable=wrong-import-position
+SHAKESPEARE_TXT = '/home/alfredoavendano/content/shakespeare.txt'
 
-# Cloud TPU Cluster Resolver flags
-tf.flags.DEFINE_string(
-    "tpu", default=None,
-    help="The Cloud TPU to use for training. This should be either the name "
-    "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
-    "url.")
-tf.flags.DEFINE_string(
-    "tpu_zone", default=None,
-    help="[Optional] GCE zone where the Cloud TPU is located in. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
-tf.flags.DEFINE_string(
-    "gcp_project", default=None,
-    help="[Optional] Project name for the Cloud TPU-enabled project. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
+def transform(txt):
+  return np.asarray([ord(c) for c in txt if ord(c) < 255], dtype=np.int32)
 
-# Model specific parameters
-tf.flags.DEFINE_string("data_dir", "",
-                       "Path to directory containing the MNIST dataset")
-tf.flags.DEFINE_string("model_dir", None, "Estimator model_dir")
-tf.flags.DEFINE_integer("batch_size", 1024,
-                        "Mini-batch size for the training. Note that this "
-                        "is the global batch size and not the per-shard batch.")
-tf.flags.DEFINE_integer("train_steps", 1000, "Total number of training steps.")
-tf.flags.DEFINE_integer("eval_steps", 0,
-                        "Total number of evaluation steps. If `0`, evaluation "
-                        "after training is skipped.")
-tf.flags.DEFINE_float("learning_rate", 0.05, "Learning rate.")
+def input_fn(seq_len=100, batch_size=1024):
+  """Return a dataset of source and target sequences for training."""
+  with tf.io.gfile.GFile(SHAKESPEARE_TXT, 'r') as f:
+    txt = f.read()
 
-tf.flags.DEFINE_bool("use_tpu", True, "Use TPUs rather than plain CPUs")
-tf.flags.DEFINE_bool("enable_predict", True, "Do some predictions at the end")
-tf.flags.DEFINE_integer("iterations", 50,
-                        "Number of iterations per TPU training loop.")
-tf.flags.DEFINE_integer("num_shards", 8, "Number of shards (TPU chips).")
+  source = tf.constant(transform(txt), dtype=tf.int32)
 
-FLAGS = tf.flags.FLAGS
+  ds = tf.data.Dataset.from_tensor_slices(source).batch(seq_len+1, drop_remainder=True)
+
+  def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+  BUFFER_SIZE = 10000
+  ds = ds.map(split_input_target).shuffle(BUFFER_SIZE).batch(batch_size, drop_remainder=True)
+
+  return ds.repeat()
 
 
-def metric_fn(labels, logits):
-  accuracy = tf.metrics.accuracy(
-      labels=labels, predictions=tf.argmax(logits, axis=1))
-  return {"accuracy": accuracy}
+EMBEDDING_DIM = 512
+
+def lstm_model(seq_len=100, batch_size=None, stateful=True):
+  """Language model: predict the next word given the current word."""
+  source = tf.keras.Input(
+      name='seed', shape=(seq_len,), batch_size=batch_size, dtype=tf.int32)
+
+  embedding = tf.keras.layers.Embedding(input_dim=256, output_dim=EMBEDDING_DIM)(source)
+  lstm_1 = tf.keras.layers.LSTM(EMBEDDING_DIM, stateful=stateful, return_sequences=True)(embedding)
+  lstm_2 = tf.keras.layers.LSTM(EMBEDDING_DIM, stateful=stateful, return_sequences=True)(lstm_1)
+  predicted_char = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(256, activation='softmax'))(lstm_2)
+  return tf.keras.Model(inputs=[source], outputs=[predicted_char])
 
 
-def model_fn(features, labels, mode, params):
-  """model_fn constructs the ML model used to predict handwritten digits."""
 
-  del params
-  image = features
-  if isinstance(image, dict):
-    image = features["image"]
+tf.keras.backend.clear_session()
 
-  model = mnist.create_model("channels_last")
+resolver = tf.contrib.cluster_resolver.TPUClusterResolver(TPU_WORKER)
+tf.contrib.distribute.initialize_tpu_system(resolver)
+strategy = tf.contrib.distribute.TPUStrategy(resolver)
 
-  if mode == tf.estimator.ModeKeys.PREDICT:
-    logits = model(image, training=False)
-    predictions = {
-        'class_ids': tf.argmax(logits, axis=1),
-        'probabilities': tf.nn.softmax(logits),
-    }
-    return tf.compat.v1.estimator.tpu.TPUEstimatorSpec(mode, predictions=predictions)
+with strategy.scope():
+  training_model = lstm_model(seq_len=100, stateful=False)
+  training_model.compile(
+      optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.01),
+      loss='sparse_categorical_crossentropy',
+      metrics=['sparse_categorical_accuracy'])
 
-  logits = model(image, training=(mode == tf.estimator.ModeKeys.TRAIN))
-  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    learning_rate = tf.train.exponential_decay(
-        FLAGS.learning_rate,
-        tf.train.get_global_step(),
-        decay_steps=100000,
-        decay_rate=0.96)
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    if FLAGS.use_tpu:
-      optimizer = tf.compat.v1.tpu.CrossShardOptimizer(optimizer)
-    return tf.compat.v1.estimator.tpu.TPUEstimatorSpec(
-        mode=mode,
-        loss=loss,
-        train_op=optimizer.minimize(loss, tf.train.get_global_step()))
-
-  if mode == tf.estimator.ModeKeys.EVAL:
-    return tf.compat.v1.estimator.tpu.TPUEstimatorSpec(
-        mode=mode, loss=loss, eval_metrics=(metric_fn, [labels, logits]))
+training_model.fit(
+    input_fn(),
+    steps_per_epoch=100,
+    epochs=10
+)
+training_model.save_weights('bard.h5', overwrite=True)
 
 
-def train_input_fn(params):
-  """train_input_fn defines the input pipeline used for training."""
-  batch_size = params["batch_size"]
-  data_dir = params["data_dir"]
-  # Retrieves the batch size for the current shard. The # of shards is
-  # computed according to the input pipeline deployment. See
-  # `tf.compat.v1.estimator.tpu.RunConfig` for details.
-  ds = dataset.train(data_dir).cache().repeat().shuffle(
-      buffer_size=50000).batch(batch_size, drop_remainder=True)
-  return ds
+ #MAKE SOME PREDICTIONS FROM THE
 
 
-def eval_input_fn(params):
-  batch_size = params["batch_size"]
-  data_dir = params["data_dir"]
-  ds = dataset.test(data_dir).batch(batch_size, drop_remainder=True)
-  return ds
 
+BATCH_SIZE = 5
+PREDICT_LEN = 250
 
-def predict_input_fn(params):
-  batch_size = params["batch_size"]
-  data_dir = params["data_dir"]
-  # Take out top 10 samples from test data to make the predictions.
-  ds = dataset.test(data_dir).take(10).batch(batch_size)
-  return ds
+# Keras requires the batch size be specified ahead of time for stateful models.
+# We use a sequence length of 1, as we will be feeding in one character at a
+# time and predicting the next character.
+prediction_model = lstm_model(seq_len=1, batch_size=BATCH_SIZE, stateful=True)
+prediction_model.load_weights('/tmp/bard.h5')
 
+# We seed the model with our initial string, copied BATCH_SIZE times
 
-def main(argv):
-  del argv  # Unused.
-  tf.logging.set_verbosity(tf.logging.INFO)
+seed_txt = 'Looks it not like the king?  Verily, we must go! '
+seed = transform(seed_txt)
+seed = np.repeat(np.expand_dims(seed, 0), BATCH_SIZE, axis=0)
 
-  tpu_cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
-      FLAGS.tpu,
-      zone=FLAGS.tpu_zone,
-      project=FLAGS.gcp_project
-  )
+# First, run the seed forward to prime the state of the model.
+prediction_model.reset_states()
+for i in range(len(seed_txt) - 1):
+    prediction_model.predict(seed[:, i:i + 1])
 
-  run_config = tf.compat.v1.estimator.tpu.RunConfig(
-      cluster=tpu_cluster_resolver,
-      model_dir=FLAGS.model_dir,
-      session_config=tf.ConfigProto(
-          allow_soft_placement=True, log_device_placement=True),
-      tpu_config=tf.compat.v1.estimator.tpu.TPUConfig(
-          FLAGS.iterations, FLAGS.num_shards),
-  )
+# Now we can accumulate predictions!
+predictions = [seed[:, -1:]]
+for i in range(PREDICT_LEN):
+    last_word = predictions[-1]
+    next_probits = prediction_model.predict(last_word)[:, 0, :]
 
-  estimator = tf.compat.v1.estimator.tpu.TPUEstimator(
-      model_fn=model_fn,
-      use_tpu=FLAGS.use_tpu,
-      train_batch_size=FLAGS.batch_size,
-      eval_batch_size=FLAGS.batch_size,
-      predict_batch_size=FLAGS.batch_size,
-      params={"data_dir": FLAGS.data_dir},
-      config=run_config)
-  # TPUEstimator.train *requires* a max_steps argument.
-  estimator.train(input_fn=train_input_fn, max_steps=FLAGS.train_steps)
-  # TPUEstimator.evaluate *requires* a steps argument.
-  # Note that the number of examples used during evaluation is
-  # --eval_steps * --batch_size.
-  # So if you change --batch_size then change --eval_steps too.
-  if FLAGS.eval_steps:
-    estimator.evaluate(input_fn=eval_input_fn, steps=FLAGS.eval_steps)
+    # sample from our output distribution
+    next_idx = [
+        np.random.choice(256, p=next_probits[i])
+        for i in range(BATCH_SIZE)
+    ]
+    predictions.append(np.asarray(next_idx, dtype=np.int32))
 
-  # Run prediction on top few samples of test data.
-  if FLAGS.enable_predict:
-    predictions = estimator.predict(input_fn=predict_input_fn)
-
-    for pred_dict in predictions:
-      template = ('Prediction is "{}" ({:.1f}%).')
-
-      class_id = pred_dict['class_ids']
-      probability = pred_dict['probabilities'][class_id]
-
-      print(template.format(class_id, 100 * probability))
-
-
-if __name__ == "__main__":
-  absl_app.run(main)
+for i in range(BATCH_SIZE):
+    print('PREDICTION %d\n\n' % i)
+    p = [predictions[j][i] for j in range(PREDICT_LEN)]
+    generated = ''.join([chr(c) for c in p])  # Convert back to text
+    print(generated)
+    print()
+    assert len(generated) == PREDICT_LEN, 'Generated text too short'
