0db68304fbf1c000a2da687adb1261d931ce0e73
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-02-29 16:54:45 -0500

    Cambios

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -192,9 +192,13 @@ def create_model():
 def input_fn(batch_size=16):
     """An input_fn to parse 28x28 images from filename using tf.data."""
     # batch_size = params["batch_size"]
-    with open(PATH_TO_BATCH,"r+b") as fp:
-        state_batch,q_values = pickle.load(fp)
-    prob_dataset = tf.data.Dataset.from_tensors((state_batch, q_values))
+    with tf.io.gfile.GFile(PATH_TO_BATCH, 'r+b') as f:
+        txt = f.read()
+    source=tf.constant(txt, dtype=tf.float32)
+    print(source)
+    # with open(PATH_TO_BATCH,"r+b") as fp:
+    #     state_batch,q_values = pickle.load(fp)
+    prob_dataset = tf.data.Dataset.from_tensors(source)
 
     # batchd_prob = prob_dataset.batch(batch_size, drop_remainder=True)
     # batchd_prob =batchd_prob.cache()
