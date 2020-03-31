4d031d0c35b7e3079c1a4b181b37522e11f93a6e
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-03-30 19:08:46 -0500

    Ahora la posición es en toda la L inversa de  la posición.

--- pacman.py
+++ pacman.py
@@ -867,10 +867,10 @@ def crear_layout(dificulty):
     # La posición de la comida
     pos_comida = (10, 10)
     # Posición del pacman
-    x1 = int(np.random.randint(11, 17))
-    y1 = int(np.random.randint(2, 17))
-    x2 = int(np.random.randint(2, 17))
-    y2 =int(np.random.randint(11, 17))
+    x1 = int(np.random.randint(11, 16))
+    y1 = int(np.random.randint(2, 16))
+    x2 = int(np.random.randint(2, 16))
+    y2 =int(np.random.randint(11, 16))
     x,y=(x1,y1) if np.random.rand()>0.5 else (x2,y2)
 
     pos_pacman = (y, x)
